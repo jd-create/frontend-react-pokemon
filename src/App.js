c9407b52c4fe0d70c8361b856012ac066575aaa2
jd-create <jenniferdemeijer@t-online.de>
2021-06-14 20:26:59 +0200

    html elements for one card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
-import React from 'react';
+import axios from "axios";
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
+    const [pokemon, setPokemon] = useState([]);
+    console.log("WHAT IS THE STATE:", pokemon);
+
+    useEffect(() => {
+        console.log("ON MOUNT:");
+        async function fetchPokemon() {
+            const response = await axios.get(
+                "https://pokeapi.co/api/v2/pokemon/12/"
+        );
+            console.log("DATA:",response.data.object);
+            setPokemon("SET STATE:",response.data.object);
+        }
+
+        fetchPokemon();
+    }, []);
+
+
+
+
+return (
     <div>
-      Begin hier met de opdracht!
+        <header>
+            POKEMON:
+            <div>
+                {}
+                <div>
+                    <h1>pokemon</h1>
+                    <p className="pokemon-name" id="name">jigglypuff</p>
+                    <Link to="/icon">icon</Link>
+                    <p className="pokemon-moves" id="moves">moves: 4</p>
+                    <p className="pokemon-weight" id="weight">weight: 333</p>
+                    <p className="pokemon-abilities" id="abilities">abilities: </p>
+                    <ul className="list-abilities">
+                        <li id="first-ability">first ability</li>
+                        <li id="second-ability">second ability</li>
+                        <li id="third-ability">third ability</li>
+                    </ul>
+                </div>
+
+        </div>
+        </header>
     </div>
   );
 }
